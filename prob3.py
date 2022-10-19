72a43e5396b9d4c27a575326ec6c7a05012a98d0
Allan Navarro <anavarro3106@gmail.com>
2022-10-18 18:45:51 -0600

    Play at the end

--- prob3.py
+++ prob3.py
@@ -8,13 +8,15 @@ import time
 Fs = 44100
 print(f'Fs = {Fs} Hz')
 duration = 2
+print('recording')
 samples = sd.rec(int(duration * Fs), samplerate=Fs, channels=1, blocking=True)
-print('finished capturing')
+print('finished recording')
 
 alphas = [0.6, 0.4, 0.2]
 delays = [0.05, 0.250, 0.5]
 # y(n) = αy(n − k) + (1 − α)x(n)
 
+results = []
 """ 
 a[0]*y[n] = b[0]*x[n] + b[1]*x[n-1] + ... + b[M]*x[n-M]
                       - a[1]*y[n-1] - ... - a[N]*y[n-N]
@@ -29,8 +31,16 @@ for i in range(len(alphas)):
 
     b = np.asarray([(1-alpha)])
 
-    result = lfilter(b, a, samples)
+    results.append(lfilter(b, a, samples))
+
+    print(f'calculating α={alpha} k = Fs*{delays[i]}')
+
+for i in range(len(results)):
+    alpha = alphas[i]
+    k = int(Fs*delays[i])
+    result=results[i]
 
     print(f'playing α={alpha} k = Fs*{delays[i]}')
     sd.play(result, samplerate=Fs, blocking=True)
+    time.sleep(0.5)
 
